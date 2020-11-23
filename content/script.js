5001d4ea2cd1392081e1d025634a6b151058bea1
EmilChapchakchi <chapchakchieb@rospartner.ru>
2020-11-23 14:58:39 +0300

    Хуй знает чё делать Спрошу у Гришы

--- content/script.js
+++ content/script.js
@@ -1,1 +1,18 @@
-console.log('Hello')
+// console.log('Hi');
+
+function Waiter() {
+	// const app = document.querySelector('#app');
+	const app = $('#app');
+	if (app.length) {
+		// console.log(app);
+		app.on('click', function () {
+			console.log(this.querySelector('.payouts-block'));
+		});
+		clearInterval(interval);
+	}
+	else{
+		console.log('Бля');
+	}
+}
+
+const interval = setInterval(Waiter, 11000);
